9fab8ee2d5367308abbe3104aafc9688cecd8596
uevan2 <43913552+uevan2@users.noreply.github.com>
2021-03-14 11:27:27 -0700

    level3

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -3,31 +3,50 @@ package _00_IntroToArrays;
 import java.util.Random;
 
 public class _00_ArrayCheatSheet {
-	public static void main(String[] args) {
+	public static <intArray> void main(String[] args) {
 		//1. make an array of 5 Strings
-
+			String[] nums = new String[5];
 		//2. print the third element in the array
-
+				System.out.println(nums[2]);
 		//3. set the third element to a different value
-
+				nums[2] = "words";
 		//4. print the third element again
-		
+				System.out.println(nums[2]);
 		//5. use a for loop to set all the elements in the array to a string of your choice
-		
+				for(int i = 0; i < nums.length; i++) {
+					nums[i] = "wordsssss";
+					System.out.println(nums[i]);
+				}
 		//6. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+				
 		//7. make an array of 50 integers
-
+				int[] ints = new int[50];
+				int smallest = 999999999;
+				int largest = -999999999;
 		//8. use a for loop to make every value of the integer array a random number
-
+				Random r = new Random();
+				for(int i = 0; i < ints.length; i++) {
+					ints[i] = r.nextInt();
+					if(smallest > ints[i]) {
+						smallest = ints[i];
+						System.out.println(smallest);
+					}
+					
+					if(largest < ints[i]) {
+						largest = ints[i];
+						System.out.println(largest);
+					}
+				
+					System.out.println(ints[i]);
+				}
 		//9. without printing the entire array, print only the smallest number on the array
-
+				
 		//10 print the entire array to see if step 8 was correct
-
+				
 		//11. print the largest number in the array.
 		
 		//12. print only the last element in the array
-		
+				System.out.println(ints[49]);
 	}
 }
--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -4,23 +4,57 @@ import java.util.Random;
 
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
-	
+		public static void main(String[] args) {
+			String[] sArray = new String[5];
+			sArray[0] = "a";
+			sArray[1] = "b";
+			sArray[2] = "c";
+			sArray[3] = "d";
+			sArray[4] = "e";
+		
+			arrayPrinter(sArray);
+			arrayPrinter2(sArray);
+			arrayPrinter3(sArray);
+			arrayPrinterRandom(sArray);
+		}
 	
 	
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
 	
+	static void arrayPrinter(String[] s) {
+		for(int i = 0; i < s.length; i++) {
+			System.out.println(s[i]);
+		}
+	}
+
 	
-	
-	//3. Write a method that takes an array of Strings and prints all the Strings in the array
+//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
+	static void arrayPrinter3(String[] s) {
+		for(int i = s.length-1; i >= 0; i--) {
+			System.out.println(s[i]);
+		}
+	}
 	
 	
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
+		static void arrayPrinter2(String[] s) {
+			for(int i = 0; i < s.length; i=i+2) {
+				System.out.println(s[i]);
+			}
+		}
 	
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
-	
+	static void arrayPrinterRandom(String[] s) {
+		
+		Random r = new Random();
+		
+		for(int i = 0; i < s.length; i++) {
+			System.out.println(s[r.nextInt(s.length)]);
+		}
+		
+	}
 	
 }
